bfa986e23cd4da8e7ea0baa717ed791da5961006
EamonOCathain <clv366@alumni.ku.dk>
2025-10-22 13:10:48 +0200

    removed smoke test

--- src/dataset/smoke_Test.py
+++
@@ -1,5 +0,0 @@
-ds = CustomDataset(data_dir=..., std_dict=std_dict, tensor_type="train", exclude_vars=["foo","bar"])
-print("input_dim:", len(ds.input_order), "output_dim:", len(ds.output_order))
-print("schema sig:", ds.schema.signature())
-x, m, a = ds[0]
-print(x.shape, m.shape, a.shape)
